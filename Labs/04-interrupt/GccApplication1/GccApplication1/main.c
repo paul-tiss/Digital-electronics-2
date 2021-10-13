034bc32ff17153c3d60b6a9f49b528cd8db048ab
student <student@PC-631.urel.feec.vutbr.cz>
2021-10-13 16:48:18 +0200

    lab4

---
+++ Labs/04-interrupt/GccApplication1/GccApplication1/main.c
@@ -0,0 +1,18 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 13.10.2021 15:41:07
+ * Author : student
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
